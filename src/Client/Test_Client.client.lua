52ce3fc6e6f25558b1de5f967307dfde066c349b
Ulric <ulricaird1@gmail.com>
2023-03-15 09:24:31 -0400

    added a class parameter

--- src/Client/Test_Client.client.lua
+++ src/Client/Test_Client.client.lua
@@ -1,7 +1,7 @@
 local OneFrame = require(game:GetService("ReplicatedStorage"):WaitForChild("OneFrame"))
 
-OneFrame.Component:GetRegisteredSignal(function(name)
-    print(name, "was registered!")
+OneFrame.Component:GetRegisteredSignal(function(name, class)
+    print(name, "was registered!", class)
 end)
 
 local success = OneFrame.StartClient(game.ReplicatedStorage.Test, true)
