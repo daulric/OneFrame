d3ef4f06f5e5df9d13aad6d50c01409a205b6201
Ulric <ulricaird1@gmail.com>
2023-03-15 21:36:29 -0400

    creating test updated!

--- src/Shared/OneFrame/Signal.spec.lua
+++ src/Shared/OneFrame/Signal.spec.lua
@@ -7,26 +7,33 @@ return function ()
         local NewSignal = Signal.new()
         
         it("accept data", function()
-            NewSignal:Connect(function(...)
-                print(...)
-            end)
+            expect(function()
+                NewSignal:Connect(function(...)
+                    print(...)
+                end)
+            end).to.be.ok()
 
-            expect(NewSignal).to.be.ok()
         end)
 
         it("sends data", function()
-            NewSignal:Fire("success")
-            expect(NewSignal).to.be.ok()
+            expect(function()
+                NewSignal:Fire("success")
+            end).to.be.ok()
+
         end)
 
         it("event waits", function()
-            local wait = NewSignal:Wait()
-            expect(wait).to.be.ok()
+            expect(function()
+                NewSignal:Wait()
+            end).to.be.ok()
+
         end)
 
         it("destroy event", function()
-            NewSignal:Destroy()
-            expect(NewSignal).to.be.equal(nil)
+            expect(function()
+                NewSignal:Destroy()
+            end).to.be.ok()
+
         end)
 
     end)
