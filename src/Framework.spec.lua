50a6f8c1eb986495df2a2ce97bd6417542d3b3ff
Ulric <ulricaird1@gmail.com>
2023-03-31 05:46:43 -0400

    Update Framework.spec.lua
    changes to the test component

--- src/Framework.spec.lua
+++ src/Framework.spec.lua
@@ -1,26 +1,14 @@
 return function ()
     local Framework = require(script.Parent:WaitForChild("Framework"))
-    local RunService = game:GetService("RunService")
 
     describe("Framework Component", function()
         local testFolder = script.Parent.Parent:WaitForChild("Test")
 
-        it("Server Components Running", function()
-            if RunService:IsServer() then
-                local Start = Framework.Server(testFolder)
+        it("Components Should Run", function()
+            local Start = Framework(testFolder)
 
-                expect(Start).to.be.a("boolean")
-                expect(Start).to.be.ok() 
-            end
-        end)
-
-        it("Client Components Running", function()
-            if RunService:IsClient() then
-                local Start = Framework.Client(testFolder)
-
-                expect(Start).to.be.a("boolean")
-                expect(Start).to.be.ok()
-            end
+            expect(Start).to.be.a("boolean")
+            expect(Start).to.be.ok() 
         end)
 
     end)
