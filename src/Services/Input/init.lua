91a0092f0cc3fe786f6fd13a590fb9b1dece6036
Ulric <ulricaird1@gmail.com>
2023-05-18 07:03:18 -0400

    v0.21.0
    Removed Context Action and more

--- src/Services/Input/init.lua
+++ src/Services/Input/init.lua
@@ -3,5 +3,4 @@ local RunService = game:GetService("RunService")
 return {
     Keyboard = require(script:WaitForChild("Keyboard")),
     Mouse = require(script:WaitForChild("Mouse")),
-    ContextAction = require(script:WaitForChild("ContextAction")),
 }
